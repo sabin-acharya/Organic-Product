a96c3d5b0de9555b17ddad0be4c95f28abde2b44
Sabin <worldfootball781@gmail.com>
2025-02-16 18:22:07 +0545

    "add home folder and slider"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/header/header';
+import Slider from './Home/slider/slider'
+
 function App() {
   return (
     <>
       <Header/>
+      <Slider/>
         
     </>
   );
---
+++ src/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Slider from './slider/slider'
+
+const Home =() => {
+    return (
+      <Slider/>
+    )
+  }
+  export default Home;
